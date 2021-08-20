50a15678266bac5541d3aeaf5ff8ac7368e1317a
ming <wmraud@nate.com>
2021-08-21 05:01:48 +0900

    2021 0821 05:01 회원가입아디중복체크

--- src/main/java/com/ming/mall/DAO/MemberDAOImpl.java
+++ src/main/java/com/ming/mall/DAO/MemberDAOImpl.java
@@ -48,5 +48,11 @@ public class MemberDAOImpl implements MemberDAO {
     	int result = sql.selectOne("memberMapper.passChk", vo);
     	return result;
     }
+ // 아이디 중복 체크
+    @Override
+    public int idChk(MemberVO vo) throws Exception {
+    	int result = sql.selectOne("memberMapper.idChk", vo);
+    	return result;
+    }
 	
 }
